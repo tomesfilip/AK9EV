d89a24d6e954cebe6510317a9dd8a7a9fdf43285
Filip Tomes <filiptomes@protonmail.com>
2023-12-20 19:36:23 +0100

    benchmarking all possible functions (17)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from algorithms.de import de_best_1_bin
+from algorithms.de import de_rand_1_bin
 
 import pybenchfunction as bench
 
@@ -18,12 +19,13 @@ def plot_function_2d_3d(func, input_domain, n_space=100):
 
 
 if __name__ == '__main__':
-    INPUT_DOMAIN = ([-100, 100], [-100, 100])
+    INPUT_DOMAIN = ([-100, 100], [-100, 100])  # Used for plotting
+    DIMENSIONS = 10  # Could be: 2, 10, 30
 
-    # get all the available functions accepting ANY dimension
+    # get all the available functions accepting defined DIMENSIONS
     any_dim_functions = bench.get_functions(None)
     continous_multimodal_nonconvex_2d_functions = bench.get_functions(
-        2,  # dimension
+        d=DIMENSIONS,
         continuous=True,
         convex=None,
         separable=None,
@@ -32,43 +34,36 @@ if __name__ == '__main__':
         randomized_term=None
     )
 
-    functions_to_plot = [
-        bench.function.Ackley(2),
-        bench.function.Adjiman(2),
-        bench.function.AlpineN2(2),
-        bench.function.Beale(2),
-        bench.function.Bird(2),
-        bench.function.BohachevskyN2(2),
-        bench.function.BohachevskyN3(2),
-        bench.function.Branin(2),
-        bench.function.BukinN6(6),
-        bench.function.CrossInTray(2),
-        bench.function.DeJongN5(2),
-        bench.function.DeckkersAarts(2),
-        bench.function.Easom(2),
-        bench.function.EggCrate(2),
-        bench.function.HappyCat(2),
-        bench.function.Himmelblau(2),
-        bench.function.HolderTable(2),
-        bench.function.Keane(2),
-        bench.function.Langermann(2),
-        bench.function.LevyN13(2),
-        bench.function.McCormick(2),
-        bench.function.Michalewicz(2),
-        bench.function.Periodic(2),
-        bench.function.PermDBeta(2),
-        bench.function.Qing(2),
-        bench.function.Quartic(2),
-        bench.function.Rastrigin(2),
-        bench.function.Rosenbrock(2),
-        bench.function.Salomon(2),
-        bench.function.Schwefel(2),
-        bench.function.Shubert(2),
-        bench.function.ShubertN3(2),
-        bench.function.ShubertN4(2),
-        bench.function.StyblinskiTank(2),
-        bench.function.Thevenot(2),
+    bench_functions = [
+        bench.function.Ackley(DIMENSIONS),
+        bench.function.HappyCat(DIMENSIONS),
+        bench.function.Michalewicz(DIMENSIONS),
+        bench.function.Periodic(DIMENSIONS),
+        bench.function.PermDBeta(DIMENSIONS),
+        bench.function.Qing(DIMENSIONS),
+        bench.function.Quartic(DIMENSIONS),
+        bench.function.Rastrigin(DIMENSIONS),
+        bench.function.Rosenbrock(DIMENSIONS),
+        bench.function.Salomon(DIMENSIONS),
+        bench.function.Schwefel(DIMENSIONS),
+        bench.function.Shubert(DIMENSIONS),
+        bench.function.ShubertN3(DIMENSIONS),
+        bench.function.ShubertN4(DIMENSIONS),
+        bench.function.StyblinskiTank(DIMENSIONS),
+        bench.function.Thevenot(DIMENSIONS),
     ]
 
-    for func in functions_to_plot:
-        plot_function_2d_3d(func, INPUT_DOMAIN)
+    print('TOTAL FUNCTIONS:')
+    print(len(bench_functions))
+
+    bounds = [(-100, 100) for _ in range(DIMENSIONS)]
+    for func in bench_functions:
+        print('BEST: ' + func.name)
+        for result in de_best_1_bin(func, bounds):
+            print(result)
+
+        print('\nRANDOM: ' + func.name)
+        for result in de_rand_1_bin(func, bounds):
+            print(result)
+
+    # plot_function_2d_3d(func, INPUT_DOMAIN)
